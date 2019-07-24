25acb6f933623daa9eb2da4f753babae5f041bab
Duy Khang <khangemail22@gmail.com>
2019-07-24 10:42:16 +0700

    init everything...

--- index.js
+++ index.js
@@ -3,20 +3,18 @@ const app = express();
 const ip = require('ip');
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static("./public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-app.set("view engine","ejs");
-app.set("views","./views");
-app.get('/', (req, res) => {  
-  res.render('index', { title:'The index page!' });
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-server.listen(3000, () => console.log('Example app listening on port 3000!' + ip.address()));
-
-io.on("connection",(socket)=>{
-  console.log('Co nguoi dang nhap:' + socket.id);
-  socket.on("xin chao",() => {
-    console.log('Xin chao');
-  });
+io.on("connection", (socket) => {
+    console.log(`Hello ${socket.id}`)
 });
+
+server.listen(PORT, () => console.log(`Example app listening on port 3000! IP Address:  ${ip.address()}`));
